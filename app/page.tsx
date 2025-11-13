ec360f0493fbdcf38702db50f7741a27f3873a6b
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-11-13 17:07:15 +0700

    Setup project and UI

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+
+const Home = () => {
+    return (
+        <div className="p-10 bg-shop-light-pink">
+            <h2 className="text-xl font-semibold">home</h2>
+            <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, cum. Amet iusto cupiditate laborum quis delectus, quaerat, odit eveniet totam nisi earum cumque blanditiis, illum optio neque debitis commodi alias!
+            </p>
+            <Button >Check out </Button>
+        </div >
+    )
+}
+export default Home;
