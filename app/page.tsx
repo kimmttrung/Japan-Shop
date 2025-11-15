15142cb2dc4d04256710fba5391fed033345ab7f
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-11-15 17:20:59 +0700

    design Logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
+
+import Container from "@/components/Container";
 import { Button } from "@/components/ui/button";
 
 
 const Home = () => {
     return (
-        <div className="p-10 bg-shop-light-pink">
+        <Container className="p-10 bg-shop-light-pink">
             <h2 className="text-xl font-semibold">home</h2>
             <p>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, cum. Amet iusto cupiditate laborum quis delectus, quaerat, odit eveniet totam nisi earum cumque blanditiis, illum optio neque debitis commodi alias!
             </p>
             <Button >Check out </Button>
-        </div >
+        </Container>
     )
 }
 export default Home;
