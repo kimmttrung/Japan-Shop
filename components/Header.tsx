1fa1d89627979dbb678d4d3caecacdedabdea7bb
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-11-18 23:25:08 +0700

    cretae search and cart icon and favorite

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Container from './Container'
 import Logo from './Logo'
 import HeaderMenu from './HeaderMenu'
+import SearchBar from './SearchBar'
+import CartIcon from './CartIcon'
+import FavoriteButton from './FavoriteButton'
 
 const Header = () => {
     return (
@@ -9,7 +12,11 @@ const Header = () => {
             <Container className="flex items-center justify-between">
                 <Logo />
                 <HeaderMenu />
-                <div>Login</div>
+                <div className='w-auto md:w-1/3 flex items-center justify-end gap-5'>
+                    <SearchBar />
+                    <CartIcon />
+                    <FavoriteButton />
+                </div>
             </Container>
         </header>
     )
