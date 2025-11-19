1eb064673f8414e9ddb04d323a04d915b908f46b
kimmttrung <160985982+kimmttrung@users.noreply.github.com>
2025-11-19 10:49:58 +0700

    complete create reposive header

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,17 +5,23 @@ import HeaderMenu from './HeaderMenu'
 import SearchBar from './SearchBar'
 import CartIcon from './CartIcon'
 import FavoriteButton from './FavoriteButton'
+import SignIn from './SignIn'
+import MoblieMenu from './MoblieMenu'
 
 const Header = () => {
     return (
         <header className="bg-white py-5 border-b border-b-black/20">
-            <Container className="flex items-center justify-between">
-                <Logo />
+            <Container className="flex items-center justify-between text-lightColor">
+                <div className='w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0'>
+                    <MoblieMenu />
+                    <Logo />
+                </div>
                 <HeaderMenu />
                 <div className='w-auto md:w-1/3 flex items-center justify-end gap-5'>
                     <SearchBar />
                     <CartIcon />
                     <FavoriteButton />
+                    <SignIn />
                 </div>
             </Container>
         </header>
